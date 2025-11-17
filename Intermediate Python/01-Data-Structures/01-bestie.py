1241494aa18acfa745d8899f1dd8fa65af485557
DakotaK92 <dakotaking92@gmail.com>
2025-11-17 14:45:18 -0500

    Little notes to python part 2 folder

---
+++ Intermediate Python/01-Data-Structures/01-bestie.py
@@ -0,0 +1,2 @@
+genz_slang = ['67', 'busen', 'gang-gang']
+print(genz_slang)
---
+++ Intermediate Python/01-Data-Structures/02-find_my_friends.py
@@ -0,0 +1,9 @@
+friend1 = (32.9515472, -96.9034244)
+friend2 = (32.9515472, -96.9034244)
+friend3 = (32.9515472, -96.9034244)
+
+print("Friend 1 is at latitude:", friend1[0], "and longitude:", friend1[1])
+
+all_friends_locations = friend1 + friend2 + friend3
+
+print("All friends' locations:", all_friends_locations)
